d87f367de8117abcf5c8faee73a0db6ee68ec24e
thientq2501 <>
2024-05-04 11:01:49 +0700

    feat: log port

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,6 @@ async function bootstrap() {
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
+bootstrap().then(() =>
+  console.log(`Application is running on: ${process.env.PORT || 3000}`),
+);
